c8fb35501120d9281857db6ebb25fc3e40307761
Sudaisneelambra <sudaisanuneelambra21@gmail.com>
2024-06-12 15:34:10 +0530

    complettred

--- routes/adminroutes.js
+++ routes/adminroutes.js
@@ -1,14 +1,14 @@
 const express =require('express')
 const router = express.Router()
 const admin = require('../controllers/admincontroller')
+const tokencheck = require('../middlewares/tockencheck')
 
-
-router.post('/adduser', admin.addUser)
-router.get('/showUsers', admin.showUser)
-router.patch('/changeStatus', admin.blockAndUnblockUser)
-router.post('/addshop', admin.addShop)
-router.get('/showShops', admin.ShowShops)
-router.patch('/deleteUndoShops', admin.deleteAndUndoShops)
+router.post('/adduser',tokencheck, admin.addUser)
+router.get('/showUsers',tokencheck, admin.showUser)
+router.patch('/changeStatus',tokencheck, admin.blockAndUnblockUser)
+router.post('/addshop',tokencheck, admin.addShop)
+router.get('/showShops',tokencheck, admin.ShowShops)
+router.patch('/deleteUndoShops',tokencheck, admin.deleteAndUndoShops)
 
 
 

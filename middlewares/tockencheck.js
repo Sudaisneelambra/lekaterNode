fa4e4a4dc8732775c078588ba308848bdf8f0a69
Sudaisneelambra <sudaisanuneelambra21@gmail.com>
2024-06-13 12:23:56 +0530

    try catch implemented on token check

--- middlewares/tockencheck.js
+++ middlewares/tockencheck.js
@@ -1,24 +1,33 @@
 const jwt = require('jsonwebtoken');
 
 const checkTocken=(req, res, next)=>{
-  const secretKey= process.env.JWT_SECRETKEY;
-  const authorizationHeader = req.headers['authorization'];
-  const token = authorizationHeader.replace('Bearer ', '');
-  jwt.verify(token, secretKey, (err, decoded) => {
-    if (err) {
-      res.json({message: `verification fialed due to  ${err.message}`, expiry: err.message});
-    } else {
-      const expirationTime = decoded.exp;
-      const currentTime = Math.floor(Date.now() / 1000);
+  try{
+      const secretKey= process.env.JWT_SECRETKEY;
+      const authorizationHeader = req.headers['authorization'];
+      const token = authorizationHeader.replace('Bearer ', '');
+      jwt.verify(token, secretKey, (err, decoded) => {
+        if (err) {
+          res.json({message: `verification fialed due to  ${err.message}`, expiry: err.message});
+        } else {
+          const expirationTime = decoded.exp;
+          const currentTime = Math.floor(Date.now() / 1000);
 
-      if (expirationTime < currentTime) {
-        console.log('Authorization header has expired');
-      } else {
-        req.tokens= decoded;
-        next();
-      }
-    }
-  });
+          if (expirationTime < currentTime) {
+            console.log('Authorization header has expired');
+          } else {
+            req.tokens= decoded;
+            next();
+          }
+        }
+      });
+  }
+  catch (err){
+      console.log(err);
+      res.status(401).json({
+        message: 'Unauthorized',
+        error: err.message
+      })
+  }
 };
 
 module.exports=checkTocken;
